d05cf351c25d329e23ca491aeaaab249ec22abae
Marco30 <mv222fp@student.lnu.se>
2014-10-10 21:10:16 +0200

    Lab2.2 start

---
+++ 1DV402.S2.L2A/1DV402.S2.L2A/AlarmClock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L2A
+{
+    class AlarmClock
+    {
+        private int _alarmHour;
+
+        private int _alarmMinute;
+
+        private int _hour;
+
+        private int _minute;
+
+    }
+}
